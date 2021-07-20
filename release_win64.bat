47b1d5bf00dea77a59b876ece1b1d5c7bedc1af0
Marladon <skyllann7@gmail.com>
2021-07-20 15:46:39 +0300

    rename .ico

--- release_win64.bat
+++ release_win64.bat
@@ -11,10 +11,10 @@ venv\Scripts\python -m pip install --upgrade pip
 venv\Scripts\python -m pip install -r requirements.txt
 
 venv\Scripts\python -m pip install pyinstaller
-venv\Scripts\pyinstaller -i "usbadc10gui/usbadc10.ico" --add-data "usbadc10gui/usbadc10.ico;." --clean -F --noconsole --add-binary "usbadc10gui/usbadc10-win64/usbadc10.dll;." usbadc10gui/__main__.py -n UALab
+venv\Scripts\pyinstaller -i "usbadc10gui/UALab.ico" --add-data "usbadc10gui/UALab.ico;." --clean -F --noconsole --add-binary "usbadc10gui/usbadc10-win64/usbadc10.dll;." usbadc10gui/__main__.py -n UALab
 
 move dist release_win64
 if exist build rd /s/q build
 if exist dist rd /s/q dist
 if exist __main__.spec del __main__.spec
-copy usbadc10gui\usbadc10.ico release_win64
+copy usbadc10gui\UALab.ico release_win64
